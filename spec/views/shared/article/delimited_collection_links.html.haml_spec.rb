370a349b52bc47f636aa8eac227b47bcaad355da
Charlie Hawker <charlie.hawker@gmail.com>
2018-04-06 14:41:05 +0100

    Fix failing specs since refactor

--- spec/views/shared/article/delimited_collection_links.html.haml_spec.rb
+++ spec/views/shared/article/delimited_collection_links.html.haml_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'collections/_delimited_links' do
+RSpec.describe 'shared/article/_delimite_collection_links' do
   let!(:collections) {
-    assign(:collections,
+    assign(:links,
       [
         double(:collection,
-          name:     collection_name_text1,
+          name:     collection_link_name_text1,
           graph_id: 'xxxxxxx1'
         ),
         double(:collection,
-          name:     collection_name_text2,
+          name:     collection_link_name_text2,
           graph_id: 'xxxxxxx2'
         ),
       ]
     )
   }
 
-  let!(:collection_name_text1) { 'This is test collection 1.' }
-  let!(:collection_name_text2) { 'This is test collection 2.' }
+  let!(:collection_link_name_text1) { 'This is test collection 1.' }
+  let!(:collection_link_name_text2) { 'This is test collection 2.' }
 
   before(:each) do
-    render partial: "collections/delimited_links", locals: { collections: collections }
+    render partial: "shared/article/delimited_collection_links", locals: { links: collections }
   end
 
   context 'collection' do
@@ -31,7 +31,7 @@ RSpec.describe 'collections/_delimited_links' do
     end
 
     context 'sanitize' do
-      let!(:collection_name_text1) { '<script>This is test collection 1.</script>' }
+      let!(:collection_link_name_text1) { '<script>This is test collection 1.</script>' }
 
       it 'sanitized name will render correctly' do
         expect(rendered).to match(/<a href="\/collections\/xxxxxxx1">This is test collection 1.<\/a>/)
@@ -41,7 +41,7 @@ RSpec.describe 'collections/_delimited_links' do
 
   context 'listing collections' do
     it 'renders comma separated links to each collection' do
-      expect(rendered).to include("<p>\n<a href=\"/collections/xxxxxxx1\">This is test collection 1.</a>\n, \n<a href=\"/collections/xxxxxxx2\">This is test collection 2.</a>\n\n</p>\n")
+      expect(rendered).to include("<a href=\"/collections/xxxxxxx1\">This is test collection 1.</a>\n, \n<a href=\"/collections/xxxxxxx2\">This is test collection 2.</a>")
     end
   end
 
---
+++ spec/views/shared/article/footer.html.haml_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'shared/article/_footer' do
+  let!(:collections) {
+    assign(:collections,
+      [
+        double(:collection,
+          name:     'Collection name',
+          graph_id: 'xxxxxxx1'
+        ),
+        double(:collection,
+          name:     'Another collection name',
+          graph_id: 'xxxxxxx2'
+        ),
+      ]
+    )
+  }
+
+  before(:each) do
+    render partial: "shared/article/footer", locals: { collections: collections }
+  end
+
+  context 'footer' do
+    context 'when collections exist' do
+      it "will render 'up to' text" do
+        expect(rendered).to match(/Up to/)
+      end
+    end
+
+    context 'when collections do not exist' do
+      let!(:collections) {
+        assign(:collections, [])
+      }
+      it "will not render 'up to' text" do
+        expect(rendered).not_to match(/Up to/)
+      end
+    end
+  end
+end
